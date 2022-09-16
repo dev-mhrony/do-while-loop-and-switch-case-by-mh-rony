8d9807a33bbde4fae7248188557f13f2e8cbb08c
MH-RONY <mh.rony.dhanvi@gmail.com>
2022-09-16 20:35:20 +0600

    Last update - 2022

---
+++ main.dart
@@ -0,0 +1,29 @@
+void main() {
+  // ----------------------------Using Do While Loop ----------------------------------------
+
+  int n = 1; // Defolt Number usint n variable
+  do {
+    // Fastoffel work the program
+    print(n); // Print number
+    print('Dart'); // Print Text
+    n++; // Incremint
+  } while (n <= 17); // using requerment / Function
+
+// ------------------------------ Using Switch Case -----------------------------------------
+  String value = "a";
+  switch (value) {
+    case "a":
+      print('This is a "A"'); // if function is a match to print this
+      break;
+    case "b":
+      print('This is a "B"'); // if function is a match to print this
+      break;
+    case "c":
+      print('This is a "C"'); // if function is a match to print this
+      break; // Program Break
+
+    default:
+      print(
+          "Doesn't Match The Input Value"); // if is a Doesn't Match to print this Value
+  }
+}
